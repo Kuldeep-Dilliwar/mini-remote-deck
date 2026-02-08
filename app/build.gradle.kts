5a5adcaf186f4b745c7cda290579983b9931c6df
Kuldeep-Dilliwar <kuldeepdilliwar@gmail.com>
2026-02-08 11:09:52 +0530

    config for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+@file:Suppress("UnstableApiUsage") // <--- THIS FIXES THE NEW WARNING
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,34 +8,60 @@ plugins {
 
 android {
     namespace = "mini.remote.deck.project.hobby"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "mini.remote.deck.project.hobby"
         minSdk = 23
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 36
+        val envVersionName = System.getenv("BUILD_VERSION_NAME") ?: "1.0-Dev"
+        val timestamp = (System.currentTimeMillis() / 60000).toInt()
+
+        versionCode = timestamp
+        versionName = envVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    // NEW WAY: This replaces 'resourceConfigurations'
+    androidResources {
+        localeFilters += "en"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("x86", "x86_64", "armeabi-v7a", "arm64-v8a")
+            isUniversalApk = true
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
     }
@@ -63,4 +91,10 @@ dependencies {
     implementation(libs.okhttp)
     implementation(libs.androidx.navigation.compose) // Use the latest version available
     implementation(libs.androidx.material.icons.extended) // Use the latest version
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+    }
 }
